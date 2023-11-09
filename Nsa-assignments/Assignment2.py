2ab3c522dadd8dbb9a0e330fe005318eb23732f0
TheePrime <107242380+TheePrime@users.noreply.github.com>
2023-11-09 12:43:14 +0300

    Create Assignment2.py
    simple calculator

---
+++ Nsa-assignments/Assignment2.py
@@ -0,0 +1,46 @@
+# Function to add two numbers
+def add(x, y):
+    return x + y
+
+# Function to subtract two numbers
+def subtract(x, y):
+    return x - y
+
+# Function to multiply two numbers
+def multiply(x, y):
+    return x * y
+
+# Function to divide two numbers
+def divide(x, y):
+    if y == 0:
+        return "Division by zero is not allowed"
+    return x / y
+
+# Main calculator loop
+while True:
+    print("Options:")
+    print("Enter 'add' for addition")
+    print("Enter 'subtract' for subtraction")
+    print("Enter 'multiply' for multiplication")
+    print("Enter 'divide' for division")
+    print("Enter 'quit' to end the program")
+
+    user_choice = input(": ")
+
+    if user_choice == "quit":
+        break
+
+    if user_choice in ("add", "subtract", "multiply", "divide"):
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
+
+        if user_choice == "add":
+            print("Result:", add(num1, num2),"\n")
+        elif user_choice == "subtract":
+            print("Result:", subtract(num1, num2))
+        elif user_choice == "multiply":
+            print("Result:", multiply(num1, num2))
+        elif user_choice == "divide":
+            print("Result:", divide(num1, num2))
+    else:
+        print("Invalid input")
